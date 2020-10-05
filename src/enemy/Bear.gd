9f43594662e1a3e05a2617994eaf9acf429804b8
Benjamin Schmid <git@schmidb.ch>
2020-10-05 13:16:13 +0200

    fix bear

--- src/enemy/Bear.gd
+++ src/enemy/Bear.gd
@@ -15,8 +15,7 @@ func on_reset():
 	.on_reset()
 
 func _ready():
-	initial_health = 120
-	health = initial_health
+	initial_health = health
 	on_reset()
 	get_tree().root.get_child(0).play_music("res://msc/CursedMeadow.ogg", true)
 
